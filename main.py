525df60ef25d0a3a1a215b08a5202ebf3e667b74
owner <j13thompson@student.bridgew.edu>
2022-12-05 19:52:27 -0500

    typo fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ lettersUsed = {}
 def count_letters(inputString):
     for i in inputString:
         if i != " ":
-            # if the letter is not found in lettersUsed, it is initialized as 0, else it is incrememnted
+            # if the letter is not found in lettersUsed, it is initialized as 0, else it is incremented
             lettersUsed[i] = lettersUsed.get(i, 0) + 1
     print(lettersUsed)
 
