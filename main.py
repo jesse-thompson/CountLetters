79e065c0ffecf224edc858348064830f352c6174
owner <j13thompson@student.bridgew.edu>
2022-12-05 19:50:52 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+userString = input("Please input a string\n")
+lettersUsed = {}
+
+
+def count_letters(inputString):
+    for i in inputString:
+        if i != " ":
+            # if the letter is not found in lettersUsed, it is initialized as 0, else it is incrememnted
+            lettersUsed[i] = lettersUsed.get(i, 0) + 1
+    print(lettersUsed)
+
+
+count_letters(userString)
